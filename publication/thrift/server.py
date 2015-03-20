54aef2dfb66e1014cf8fe6ab69d854207201417f
fabiobatalha <fabiobatalha@gmail.com>
2015-03-20 10:27:33 -0300

    Ajustes para padronizar serviço thrift

--- publication/thrift/server.py
+++ publication/thrift/server.py
@@ -5,40 +5,18 @@ import logging
 import os
 import sys
 
-from controller import stats, ServerError
+from publication.controller import stats, ServerError
 
 import thriftpy
-from thriftpy.rpc import make_server
+from thriftpy.thrift import TProcessor
 
+
+logger = logging.getLogger(__name__)
 
 publication_stats_thrift = thriftpy.load(
     os.path.dirname(__file__)+'/publication_stats.thrift',
     module_name='publication_stats_thrift'
 )
-
-DEFAULT_HOST = '127.0.0.1'
-DEFAULT_PORT = '11600'
-
-def _config_logging(logging_level='INFO', logging_file=None):
-
-    allowed_levels = {
-        'DEBUG': logging.DEBUG,
-        'INFO': logging.INFO,
-        'WARNING': logging.WARNING,
-        'ERROR': logging.ERROR,
-        'CRITICAL': logging.CRITICAL
-    }
-
-    logging_config = {
-        'level': allowed_levels.get(logging_level, 'INFO'),
-        'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    }
-
-    if logging_file:
-        logging_config['filename'] = logging_file
-
-    logging.basicConfig(**logging_config)
-
 
 class Dispatcher(object):
 
@@ -137,58 +115,4 @@ class Dispatcher(object):
         return [publication_stats_thrift.aggs(key=key, count=count) for key, count in data.items()]
 
 
-def main(host=DEFAULT_HOST, port=DEFAULT_PORT):
-    server = make_server(
-        publication_stats_thrift.PublicationStats,
-        Dispatcher(),
-        host,
-        port
-    )
-
-    logging.info('Starting Server on %s:%s' % (host, port))
-
-    server.serve()
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        description="RPC Server for SciELO Publication Statistics"
-    )
-
-    parser.add_argument(
-        '--host',
-        '-i',
-        default=DEFAULT_HOST,
-        help='RPC Server host'
-    )
-
-    parser.add_argument(
-        '--port',
-        '-p',
-        default=DEFAULT_PORT,
-        help='RPC Server port'
-    )
-
-    parser.add_argument(
-        '--logging_file',
-        '-o',
-        default='/var/log/rpc_publication_statistics.log',
-        help='Full path to the log file'
-    )
-
-    parser.add_argument(
-        '--logging_level',
-        '-l',
-        default='DEBUG',
-        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-        help='Logggin level'
-    )
-
-    args = parser.parse_args()
-
-    _config_logging(args.logging_level, args.logging_file)
-
-    main(
-        host=args.host,
-        port=args.port
-    )
+app = TProcessor(publication_stats_thrift.PublicationStats, Dispatcher())
