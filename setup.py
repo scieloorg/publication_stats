e7a33e2c0253202f5037af813ecddb2632e9bc24
fabiobatalha <fabiobatalha@gmail.com>
2016-08-17 17:20:34 -0300

    Ajustes de tag

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requires = []
 
 setup(
     name="publication",
-    version='0.5.4',
+    version='0.5.5',
     description="A SciELO RPC server and API to retrieve publication statistics from the SciELO Network ",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
