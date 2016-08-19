5804e84460ae1a2f3ca76fb2fdd9880061d72465
fabiobatalha <fabiobatalha@gmail.com>
2016-08-19 15:33:54 -0300

    Docker files

--- docker/generate_production_ini.py
+++ docker/generate_production_ini.py
@@ -3,14 +3,13 @@ import os
 from configparser import ConfigParser
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-WEBAPP_PORT = '8000'  # ver ports: docker-compose.yml e Dockerfile
 production_template_ini_filepath = os.path.join(PROJECT_PATH + '/production.ini-TEMPLATE')
 new_production_ini_filepath = os.path.join(PROJECT_PATH + '/production.ini')
 config = ConfigParser()
 config.read_file(open(production_template_ini_filepath))
-config.set('app:main', 'elasticsearch', os.environ.get('ELASTICSEARCH', 'esd.scielo.org:9200'))
-config.set('app:main', 'articlemeta', os.environ.get('ARTICLEMETA', 'esd.scielo.org:9200'))
-config.set('server:main', 'port', WEBAPP_PORT)
+config.set('app:main', 'elasticsearch', os.environ.get('ELASTICSEARCH', '127.0.0.1:9200'))
+config.set('app:main', 'articlemeta', os.environ.get('ARTICLEMETA', 'articlemeta.scielo.org:11720'))
+config.set('server:main', 'port', '8000')
 
-with open(new_production_ini_filepath, 'w') as configfile:    # save
+with open(new_production_ini_filepath, 'w') as configfile:
     config.write(configfile)
