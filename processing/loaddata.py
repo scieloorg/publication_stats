9f14744f6cf76767517fc50376e7ed3afe9c0fbc
fabiobatalha <fabiobatalha@gmail.com>
2017-07-21 15:15:12 -0300

    Ajuste em dados de periódicos

--- processing/loaddata.py
+++ processing/loaddata.py
@@ -80,6 +80,7 @@ def fmt_journal(document):
     data['issn'] = document.scielo_issn
     data['collection'] = document.collection_acronym
     data['subject_areas'] = document.subject_areas
+    data['is_multidisciplinary'] = 1 if len(document.subject_areas) > 2 else 0
     data['included_at_year'] = document.creation_date[0:4]
     data['status'] = document.current_status
     data['title'] = document.title
@@ -167,8 +168,7 @@ def fmt_document(document):
     data['processing_date'] = document.processing_date
     data['publication_date'] = document.publication_date
     data['publication_year'] = document.publication_date[0:4]
-    subject_areas = document.journal.subject_areas or ['undefined']
-    data['subject_areas'] = [i for i in subject_areas]
+    data['subject_areas'] = subject_areas = document.journal.subject_areas or ['undefined']
     data['is_multidisciplinary'] = 1 if len(subject_areas) > 2 else 0
     wos_subject_areas = document.journal.wos_subject_areas or ['undefined']
     data['wos_subject_areas'] = wos_subject_areas
