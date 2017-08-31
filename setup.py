ac926262853ffcbfca43080468ed477a32c67476
fabiobatalha <fabiobatalha@gmail.com>
2017-08-31 17:53:13 -0300

    Atualização de versão de articlemetaapi

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ install_requires = [
     'pyramid_chameleon',
     'pyramid_mako',
     'waitress',
-    'articlemetaapi==1.16.0',
-    'xylose==1.28.0',
+    'articlemetaapi==1.17.0'
     'raven'
     ]
 
@@ -28,7 +27,7 @@ test_requires = []
 
 setup(
     name="publication",
-    version='1.27.0',
+    version='1.28.0',
     description="A SciELO RPC server and API to retrieve publication statistics from the SciELO Network ",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
